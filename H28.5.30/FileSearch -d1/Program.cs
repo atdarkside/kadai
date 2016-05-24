5a76645d4e0e62930a5aed91022008de253caaf1
TATa <pf35301@gmail.com>
2016-05-25 04:34:29 +0900

    ファイル検索(発展1)

---
+++ H28.5.30/FileSearch -d1/Program.cs
@@ -0,0 +1,53 @@
+﻿//Coder :TATa
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileSearch__d1 {
+    class Program {
+        static void Main(string[] args) {
+            FileStream fs = File.OpenRead("B:YOMIGANA_SORT_TENPO.txt");
+            StreamReader sr = new StreamReader(fs,Encoding.GetEncoding("Shift_JIS"));
+            long startPos = fs.Length / 42 * 3;
+            fs.Seek(startPos,SeekOrigin.Begin);
+
+            Console.Write("支店番号:");
+            int searchNO = int.Parse(Console.ReadLine());
+
+            string line;
+            while(true) {
+                line = sr.ReadLine();
+                
+                int NO = new int();
+                
+                try {
+                    NO = int.Parse(line.Substring(1, 2));
+                }
+                catch {
+                    Console.WriteLine("その支店番号は存在しません。");
+                    break;
+                }
+                
+                
+                if(NO == searchNO) {
+                    Console.WriteLine("┌─┬─────┬──────────┬─────┬───────────────┬───────┐");
+                    Console.WriteLine("│NO│支店名    │よみがな            │郵便番号  │住所                          │電話番号      │");
+                    Console.WriteLine("├─┼─────┼──────────┼─────┼───────────────┼───────┤");
+                    Console.WriteLine(line);
+                    Console.WriteLine("└─┴─────┴──────────┴─────┴───────────────┴───────┘");
+                    break;
+                }
+                
+                /*
+                string str = line.Substring(1,2);
+                Console.WriteLine(str);
+                break;
+                */
+            }
+            Console.ReadKey();
+        }
+    }
+}
